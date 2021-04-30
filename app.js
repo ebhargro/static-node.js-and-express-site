161b00418d71a1e6dfb21aad70eb16d18965b334
Ebony Hargro <ebonyhargro@intuitdep02569.lan>
2021-04-30 10:41:22 -0700

    Fixing file paths to images

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.set('view engine', 'pug');
 
 //Using a static route and the express.static method to serve te static files located in the public folder
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
 //Setting up routes
     //Index route
@@ -31,14 +31,7 @@ app.get('/project/:id', (req, res) => {
     if (project) {
         res.locals.data = projects;
         res.render('project', {project});
-    } else {
-        const undefinedError = new Error();
-        undefinedError.status = 404;
-        undefMessage = "Sorry, but you have navigated to a non-existent page!";
-        undefinedError.message = undefMessage;
-        console.log(`${undefMessage} Error Status: ${undefinedError.status}.`);
-        next(undefinedError);
-        }
+    }    
     });
     
 // });
@@ -48,10 +41,20 @@ app.listen(3000, () => {
     console.log('This app is running on localhost:3000.');
 });
 
+//Handling errors - 404 error
+app.use((err, req, res, next) => {
+    const undefinedError = new Error();
+    undefinedError.status = 404;
+    undefMessage = "Sorry, but you have navigated to a non-existent page!";
+    undefinedError.message = undefMessage;
+    console.log(`${undefMessage} Error Status: ${undefinedError.status}.`);
+    next(undefinedError);
+});
+
 // //Handling errors - global error 
 app.use((err, req, res, next) => {
     err.status = 500;
     const globalErr = "Looks like there is an error here. 😱";
     err.message = globalErr;
-    res.render(err.status);
+    res.render(err.message);
 });
